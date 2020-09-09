e86aa1fb6c00c6f171c6d4006f31446df6f9c3bc
erica kim <gayoning0624@gmail.com>
2020-09-09 16:45:27 +1200

    add/browse htmlint

--- src/RouteAdd.js
+++ src/RouteAdd.js
@@ -1,13 +1,71 @@
 import React, {Component} from 'react'
 import RouteAdd from './add'
 
-class RouteOthersProfile extends Component {
+class RouteAdd extends Component {
 
 
 
     render(){
         return(
-            <div>Hi</div>
+            <div class="layer add">
+            <div class="main-header">
+                <div class="headerback"><i class="fas fa-chevron-left"></i><i class="fas fa-chevron-left"></i></div>
+                <img src="assets/logo-black.png" alt="logoimg" class="headerlogo">
+            </div>
+            <div class="main-add">
+                <div class="subtitle">
+                    <h1>Add New Listing</h1>
+                </div>
+                <div class="productimage">
+                    <h3>Product Image</h3>
+                    <label for="upload_file" class="custom-file-upload"><i class="fas fa-camera-retro"></i></label>
+                    <input type="file" name="upload_file" id="upload_file" >
+                    
+                </div>
+        
+                <form>
+                    <div class="form-group">
+                        <label for="product">Name</label>
+                        <input type="text" class="form-control" id="product" placeholder="Product name">
+        
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPrice">Price</label>
+                        <input type="text" class="form-control" id="exampleInputPrice" placeholder="$00.00">
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <!-- <input type="description" class="form-control" id="exampleInputdescription" placeholder="Enter Description"> -->
+                        <textarea name="description" id="description" cols="30" rows="6"></textarea>
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="shipping">Shipping Info</label>
+                        <!-- <input type="shipping" class="form-control" id="exampleInputShipping" placeholder="Enter Shipping Info"> -->
+                        <textarea name="shipping" id="shipping" cols="30" rows="5"></textarea>
+                    </div>
+                    <div class="global-btns">
+                        <button type="submit" class="delete-btn">Cancel</button>
+                        <button type="submit" class="save-btn">Add</button>
+                    </div>
+                </form>
+            </div>
+            <div class="main-footer">
+                <div class="nav home">
+                    <i class="fas fa-home"></i>
+                </div>
+                <div class="nav cart">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+                <div class="nav fav nav-on">
+                    <i class="fas fa-heart"></i>
+                </div>
+                <div class="nav profile">
+                    <i class="fas fa-user"></i>
+                </div>
+            </div>
+        </div>
         )
     }
 }
--- src/RouteBrowse.js
+++ src/RouteBrowse.js
@@ -7,8 +7,107 @@ class RouteBrowse extends Component {
 
     render (){
         return (
+            <div className="layer browse">
+                <div className="browse-header">
+                    <img src="assets/logo-black.png" alt="logoimg" className="logoimg"/>
+                    <div className="search">
+                        <button type="submit"><i className="fa fa-search"></i></button>
+                        <input type="text" className="search-bar" style="font-family:FontAwesome"/>
+                    </div>
+                </div>
+                <div className="category">
+                    <div className="category-box on">
+                        <div className="category-icon">
+                            <img src="assets/icon3.png" alt="top" className="iconimg"/>
+                        </div>
+                        <p>Top</p>
+                    </div>
+                    <div className="category-box">
+                        <div className="category-icon">
+                            <img src="assets/icon2.png" alt="bot" className="iconimg"/>
+                        </div>
+                        <p>Bottom</p>
+                    </div>
+                    <div className="category-box">
+                        <div className="category-icon">
+                            <img src="assets/icon1.png" alt="acc" className="iconimg"/>
+                        </div>
+                        <p>Acc</p>
+                    </div>
+                    <div className="category-box">
+                        <div className="category-icon">
+                            <img src="assets/icon4.png" alt="shoe" className="iconimg"/>
+                        </div>
+                        <p>Shoes</p>
+                    </div>
+                </div>
+                <div className="browse-container">
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
 
+                        </div>
+                    </div>
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
+
+                        </div>
+                    </div>
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
+
+                        </div>
+                    </div>
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
+
+                        </div>
+                    </div>
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
+
+                        </div>
+                    </div>
+                    <div className="product">
+                        <img src="assets/jacket.jpg" alt="product" className="product-img"/>
+                        <div className="pro-title">
+                            <h2>Customized Jacket</h2>
+                            <p>John Doe<br><span className="price">$75.00</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="main-footer">
+                    <div className="nav home nav-on">
+                        <i className="fas fa-home"></i>
+                    </div>
+                    <div className="nav cart">
+                        <i className="fas fa-shopping-cart"></i>
+                    </div>
+                    <div className="nav fav">
+                        <i className="fas fa-heart"></i>
+                    </div>
+                    <div className="nav profile">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+        </div>
         )
     }
 }
+
 export default RouteBrowse;
